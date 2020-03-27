0af548244a08c1ada94a79f128547f01defc0876
badtuple <the.legend@badtuple.me>
2020-03-26 23:48:35 -0700

    :facepalm: actually use incorrect msgpack in test

--- src/db/logs.rs
+++ src/db/logs.rs
@@ -31,9 +31,11 @@ impl Index<usize> for Log {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_add_valid_messagepack_msg() {
-        let log = Log::new();
+        let mut log = Log::new();
         let msg = vec![0x93, 0x00, 0x2a, 0xf7];
         if let Err(e) = log.add_msg(msg) {
             panic!("threw error for valid messagepack: {:?}", e);
@@ -42,10 +44,8 @@ mod tests {
 
     #[test]
     fn test_add_invalid_messagepack_msg() {
-        let log = Log::new();
-        // MessagePack encoded string,
-        // but bytes in string aren't valid utf8
-        let buf = vec![0xd9, 0x02, 0xc3, 0x28];
+        let mut log = Log::new();
+        let buf = vec![ 0x93, 0x00, 0x2a ];
         if let Ok(_) = log.add_msg(buf) {
             panic!("invalid messagepack was allowed into log");
         };
