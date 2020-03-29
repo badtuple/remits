17b62b06f3ae736aee0e8f7726dc3aab89cedef5
badtuple <the.legend@badtuple.me>
2020-03-29 16:27:21 -0700

    fix existing integration tests

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -50,14 +50,20 @@ async fn test_can_create_log() {
 #[tokio::test]
 async fn test_can_create_itr() {
     let mut framer = connect_to_remits().await;
-    let itr_cmd = "ITR ADD test_log test_itr \n return msg";
+
+    // happy path
+    let itr_cmd = "ITR ADD test_log test_itr map \n return msg";
     should_respond_with!(framer, itr_cmd, b"+ok");
+
+    // invalid iterator type
+    let itr_cmd = "ITR ADD test_log test_itr NOT_A_VALID_TYPE \n return msg";
+    should_respond_with!(framer, itr_cmd, b"!ItrTypeInvalid");
 }
 
 #[tokio::test]
 async fn test_malformed_msg_add() {
     let mut framer = connect_to_remits().await;
-    should_respond_with!(framer, "LOG ADD test_log", b"ok");
+    should_respond_with!(framer, "LOG ADD test_log", b"+ok");
 
     // Not valid message pack
     let msg_cmd = b"MSG ADD test_log \x93\x00\x2a".to_vec();
