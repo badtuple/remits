da25952b65d51f8064076e65dd310d31adb6033a
Andrew Scibek <andrew.scibek@gmail.com>
2020-04-16 23:05:14 -0700

    clippy worked

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use remitslib::server::run_server;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cfg = config::load();
-    1; // canary
     info!("{:?}", &cfg);
     run_server(cfg).await;
     Ok(())
